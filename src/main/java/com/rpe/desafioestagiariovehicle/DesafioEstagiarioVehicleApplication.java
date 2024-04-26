55c3a7c2cda1b3636cf7e9d30877374b0bc1bf74
Marco Túlio Duarte <marcoduarte96sm@gmail.com>
2024-04-26 18:43:00 -0300

    Criando projeto com Spring Initializr

---
+++ src/main/java/com/rpe/desafioestagiariovehicle/DesafioEstagiarioVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.rpe.desafioestagiariovehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEstagiarioVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEstagiarioVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpe/desafioestagiariovehicle/DesafioEstagiarioVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpe.desafioestagiariovehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEstagiarioVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
