027d9c3834218f57d5ad0ede1259895bab2c4a37
Marco Túlio Duarte <marcoduarte96sm@gmail.com>
2024-04-27 16:04:40 -0300

    fix: desabilitando csrf

--- src/main/java/com/rpe/desafioestagiariovehicle/security/SecurityConfiguration.java
+++ src/main/java/com/rpe/desafioestagiariovehicle/security/SecurityConfiguration.java
@@ -3,32 +3,35 @@ package com.rpe.desafioestagiariovehicle.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
+                .authorizeHttpRequests(authorize  -> authorize
                         .anyRequest().authenticated()
                 )
+                .csrf(csrf -> csrf.disable()) // Desabilitado para permitir que o usuário possa realizar operações POST
+
                 .httpBasic(withDefaults());
+
 //        http
 //                .sessionManagement(httpSecuritySessionManagementConfigurer ->
 //                httpSecuritySessionManagementConfigurer
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.sessionManagement((session) -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
+
         return http.build();
     }
 
