ff7e4bbb19bba050ac1d824ea67920ead410b4ce
Marco Túlio Duarte <marcoduarte96sm@gmail.com>
2024-04-26 21:36:41 -0300

    feat: add services

---
+++ src/main/java/com/rpe/desafioestagiariovehicle/service/VeiculoPasseioService.java
@@ -0,0 +1,17 @@
+package com.rpe.desafioestagiariovehicle.service;
+
+import com.rpe.desafioestagiariovehicle.repository.VeiculoPasseioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VeiculoPasseioService {
+
+//* Endpoint para adicionar um Veículo de Passeio
+//* Endpoint para consultar um Veículo de Passeio
+//* Endpoint para alterar um Veículo de Passeio
+//* Endpoint para remover um Veículo de Passeio
+
+
+    private VeiculoPasseioRepository repository;
+
+}
