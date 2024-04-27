a7d9c164706c4bf44767eb26c6fc4321dd563fd9
Marco Túlio Duarte <marcoduarte96sm@gmail.com>
2024-04-27 09:38:07 -0300

    fix: comentando spring security para testar funções CRUD no localhost

--- src/main/java/com/rpe/desafioestagiariovehicle/DesafioEstagiarioVehicleApplication.java
+++ src/main/java/com/rpe/desafioestagiariovehicle/DesafioEstagiarioVehicleApplication.java
@@ -1,13 +1,19 @@
 package com.rpe.desafioestagiariovehicle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesafioEstagiarioVehicleApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(DesafioEstagiarioVehicleApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Iniciando a API");
 		SpringApplication.run(DesafioEstagiarioVehicleApplication.class, args);
+		logger.info("API iniciada com sucesso");
 	}
 
 }
